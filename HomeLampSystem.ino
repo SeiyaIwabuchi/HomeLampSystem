eb5bbb1acc624a685d7744ca453cda9eae1c3ff1
SeiyaIwabuchi <0123ook@gmail.com>
2019-04-24 23:17:47 +0900

    webページからLEDの制御ができるようになった

--- HomeLampSystem.ino
+++ HomeLampSystem.ino
@@ -2,7 +2,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
 
@@ -11,30 +10,69 @@ const char* ssid = "aterm-9ac1c7-g";
 const char* password = "9a8bd40c42d12";
 
 int Year,Month,Day,Hour,Minute,Second;
+String ledState = "OFF";
+unsigned long timer=0;
 
-ESP8266WebServer server(8080); 
+ESP8266WebServer server(80);
 
 void handleRoot() {
     Serial.println("receive req: /");
-    String msg = "<h1>";
-    msg += String(Year);
-    msg += ":";
-    msg += String(Month);
-    msg += ":";
-    msg += String(Day);
-    msg += ":";
-    msg += String(Hour);
-    msg += ":";
-    msg += String(Minute);
-    msg += ":";
-    msg += String(Second);
-    msg += "</h1>";
+    String DateTime = "<h1>";
+    DateTime += String(Year);
+    DateTime += ":";
+    DateTime += String(Month);
+    DateTime += ":";
+    DateTime += String(Day);
+    DateTime += ":";
+    DateTime += String(Hour);
+    DateTime += ":";
+    DateTime += String(Minute);
+    DateTime += ":";
+    DateTime += String(Second);
+    DateTime += "</h1>";
+    String msg = "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"><title>LED Button</title></head><body>";
+    msg += "<script>";
+    msg += "function sendOn(){" ;
+    msg += "send(\"/on/\");";
+    //msg += "document.getElementById(\"LEDstatus\").innerHTML=\"ON!\";";
+    msg += "}";
+    msg += "function sendOff(){";
+    msg += "send(\"/off/\");";
+    //msg += "document.getElementById(\"LEDstatus\").innerHTML=\"OFF!\";";
+    msg += "}";
+    msg += "function send(url){";
+    msg += "var xhr = new XMLHttpRequest();";
+    msg += "xhr.open(\"GET\", url, true);";
+    msg += "xhr.send();";
+    msg += "}";
+    msg += "</script>";
+    msg += "<button id=\"on\" onClick=sendOn()>LED ON</button>";
+    msg += "<button id=\"off\" onClick=sendOff()>LED OFF</button>";
+    msg += "<h1>"+ledState+"</h1>";
+    msg += DateTime;
+    msg += "</body></html>";
     server.send(200, "text/html", msg);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
 }
 
 void getTime(){
   time_t t = now();
   t = localtime(t, 9);
+  Serial.println(t);
   Year = year(t);
   Month = month(t);
   Day = day(t);
@@ -46,78 +84,65 @@ void getTime(){
 void setup() {
   pinMode(4,OUTPUT);
   pinMode(A0,INPUT);
-  
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
+  WiFi.config(IPAddress(192,168,10,240),IPAddress(192,168,10,48),IPAddress(255,255,255,0),IPAddress(8,8,8,8));
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+    delay(500);
+    Serial.println(".");
   }
-
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready");
+  Serial.println("WIFIReady");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
   
   ntp_begin(2390); //ntp
   
   server.on("/", handleRoot); 
+  server.on("/on/", LedOn);
+  server.on("/off/", LedOff);
+  server.onNotFound(handleNotFound);
   server.begin();
+  Serial.println("ServerReady!");
 }
 
+void LedOn(){
+  Serial.println("ON");
+  digitalWrite(4,HIGH);
+  ledState = "ON";
+  server.send(200, "text/html","OK");
+  timer = millis();
+}
+
+void LedOff(){
+  Serial.println("OFF");
+  digitalWrite(4,LOW);
+  ledState = "OFF";
+  server.send(200, "text/html","OK");
+  timer = 0;
+}
+
+int nowState = 0;
 void loop() {
-  int flag=1;
-  ArduinoOTA.handle();
-  server.handleClient(); 
-  //delay(3000);
+  delay(1000);
+  server.handleClient();
+  MDNS.update();
   if(analogRead(A0) > 512){
-    if(flag == 0){
+    if(nowState == 0){
       getTime();
-      flag = 1;
+      nowState = 1;
     }
     digitalWrite(4,HIGH);
-  }else{
-    flag = 0;
+    ledState = "ON";
+  }else if(millis() - timer > 1000 * 10 * 60/*10分*/){
+    nowState = 0;
     digitalWrite(4,LOW);
+    ledState = "OFF";
   }
 }
